f3ab6381628e52d39a22c4c35b98c41f15535b18
twelww <123496256+twelww@users.noreply.github.com>
2023-03-16 08:03:04 +0300

    Create forecast.py

---
+++ forecast.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+# Replace the API key and location with your own values
+api_key = 'YOUR_API_KEY'
+location = 'New York'
+
+# Make an API request to get the weather forecast
+url = f'http://api.openweathermap.org/data/2.5/forecast?q={location}&appid={api_key}'
+response = requests.get(url)
+data = json.loads(response.text)
+
+# Extract the relevant information from the API response
+forecast_items = data['list']
+city = data['city']['name']
+
+# Print the forecast for the next few days
+print(f'Weather forecast for {city}:')
+for item in forecast_items:
+    date_time = item['dt_txt']
+    temperature = item['main']['temp']
+    description = item['weather'][0]['description']
+    print(f'{date_time}: {description}, temperature: {temperature} K')
